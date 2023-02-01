b4b6421c5bbf49e640aaaaabb6e49fce2abee130
krock13 <krock.airdrop@hotmail.com>
2023-02-01 14:59:41 +0100

    Fixed login/register function

--- js/index.js
+++ js/index.js
@@ -14,6 +14,13 @@ window.onclick = function (event) {
 
 // Register
 
+const emailRGEX = /^[\w-\.]+@([\w-]+\.)+[a-z]{2,}$/;
+const passwordRGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+const emailErrorMsg = document.getElementById('emailErrorMsg');
+const passwordErrorMsg = document.getElementById('passwordErrorMsg');
+const password2ErrorMsg = document.getElementById('password2ErrorMsg');
+const bad_login = document.getElementById('bad_login');
+
 function send(users, request) {
     fetch('http://localhost:3000/api/auth/' + request, {
         method: 'POST',
@@ -23,7 +30,14 @@ function send(users, request) {
         },
         body: users,
     })
-        .then(response => response.json())
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            if (res.status === 401) {
+                bad_login.innerText = 'Paire login/mot de passe incorrecte';
+            }
+        })
         .then(data => {
             if (data.error) {
                 alert(data.error);
@@ -37,16 +51,50 @@ function send(users, request) {
         });
 }
 
+// Email verification
+function validateEmail(email) {
+    if (emailRGEX.test(email)) {
+        emailErrorMsg.innerText = '';
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validatePassword(password) {
+    if (passwordRGEX.test(password)) {
+        emailErrorMsg.innerText = '';
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function register() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const password2 = document.getElementById('password2').value;
+    const emailField = validateEmail(email);
+    const passwordField = validatePassword(password);
+
+    if (emailField == false || passwordField == false) {
+        if (emailField == false) {
+            emailErrorMsg.innerText =
+                "L'adresse email que tu as entré n'est pas valide";
+        }
+        if (passwordField == false) {
+            passwordErrorMsg.innerText =
+                'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre';
+        }
+        return;
+    }
 
     if (email == '' || password == '' || password2 == '') {
         alert('Merci de remplir tous les champs');
     } else {
         if (password != password2) {
-            alert('Les mots de passe ne correspondent pas');
+            password2ErrorMsg.innerText =
+                'Les mots de passe ne correspondent pas';
         } else {
             const user = {
                 email: email,
